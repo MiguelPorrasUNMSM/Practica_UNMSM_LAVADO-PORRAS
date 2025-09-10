375e695398dbcc823b894fcb8313d1a8c7526ecd
Gianmarco Lavado <161788198+SirNaze0@users.noreply.github.com>
2025-09-10 11:55:26 -0500

    Add files via upload

--- Ejercicio4.kt
+++ Ejercicio4.kt
@@ -1,21 +1,10 @@
 fun main() {
-    val distanciaInicial = 400
-    val velocidadA = 80   // km/h
-    val velocidadB = 70   // km/h
-
-    var distancia = distanciaInicial.toDouble()
-    var horas = 0
-    var minutos = 0
-
-    // simulamos minuto a minuto hasta que la distancia sea 0 o negativa
-    while (distancia > 0) {
-        distancia -= (velocidadA + velocidadB) / 60.0  // avance conjunto en 1 minuto
-        minutos++
-        if (minutos == 60) {
-            horas++
-            minutos = 0
-        }
-    }
-
-    println("Los trenes se encontrarán en $horas horas y $minutos minutos.")
+    val distanciaA=80;
+    val distanciaB=70;
+    val distanciaEntreEllos=400;
+    val velocidadRelativa=distanciaA+distanciaB;
+    val tiempo = distanciaEntreEllos.toDouble() / velocidadRelativa
+    val horas = tiempo.toInt()
+    val minutos = Math.round((tiempo - horas) * 60).toInt()
+    println("Los trenes se encontrarán en "+horas+" horas y "+minutos+" minutos.")
 }
